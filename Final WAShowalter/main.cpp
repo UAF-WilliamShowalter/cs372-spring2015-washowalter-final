6196942b2b8f8d66b236a0212994dca80dc7d82c
William Showalter <williamshowalter@gmail.com>
2015-05-03 14:45:18 -0800

    Initial Commit

---
+++ Final WAShowalter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final WAShowalter
+//
+//  Created by William Showalter on 15/05/03.
+//  Copyright (c) 2015 William Showalter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}

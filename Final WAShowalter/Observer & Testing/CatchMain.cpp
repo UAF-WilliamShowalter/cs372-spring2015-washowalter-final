a9d27da4b93642d156f264c34462295ae4e1bc24
William Showalter <williamshowalter@gmail.com>
2015-05-03 16:45:07 -0800

    Added Catch for testing and added a catch target. Included in project as submodule.

---
+++ Final WAShowalter/Observer & Testing/CatchMain.cpp
@@ -0,0 +1,34 @@
+//
+//  CatchMain.cpp
+//  Final WAShowalter
+//
+//  Created by William Showalter on 15/05/03.
+//  Copyright (c) 2015 William Showalter. All rights reserved.
+//
+
+#include <stdio.h>
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <string>
+using std::string;
+
+#include <cstdio>
+using std::remove;
+
+#include <memory>
+using std::make_shared;
+using std::make_unique;
+using std::shared_ptr;
+
+
+TEST_CASE("Chain of Responsibility Firewall", "Final")
+{
+	
+	REQUIRE(0==0);
+}
